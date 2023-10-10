9df68b6145e651828068048b930ed10f9a724110
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-11 00:04:23 +0200

    Creating navigation bar,  adding style

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+    <header class="header">
+      <h1 class="logo"><a href="#">Tasty Treasures</a></h1>
+        <ul class="main-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+	</header> 
+    </div>
+  )
+}
---
+++ src/components/RegisterLogin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Home } from './Home';
+import '../components/RegisterLogin.css'
+
+
+
+export default function RegisterLogin() {
+
+
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
