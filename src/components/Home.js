ed4e2416dff7d7b3d85dfe9e15b4eac364162fd0
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-11 19:35:12 +0200

    Creating first style of Home Section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import '../components/Home.css'
 
 export const Home = () => {
   return (
     <div>
-    <header class="header">
-      <h1 class="logo"><a href="#">Tasty Treasures</a></h1>
-        <ul class="main-nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-	</header> 
+        <header class="header">
+        <h1 class="logo"><a href="#">Tasty Treasures</a></h1>
+            <ul class="main-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </header> 
+        <div class="main-banner">
+            <div class="owl-carousel owl-banner">
+                <div class="item item-1">
+                    <div class="header-text">
+                        <span class="category">Are you cooking? <em>Cook with us.</em></span>
+                        <h2> Hurry! Discover quick and tasty recipes.</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
