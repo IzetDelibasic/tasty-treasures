7d016876f0b8e3c758516c1f15dda39114346e3f
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-12 14:49:43 +0200

    Configuring login section and style for sign-up

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,14 +14,33 @@ export const Home = () => {
             </ul>
         </header> 
         <div class="main-banner">
-            <div class="owl-carousel owl-banner">
-                <div class="item item-1">
-                    <div class="header-text">
-                        <span class="category">Are you cooking? <em>Cook with us.</em></span>
-                        <h2> Hurry! Discover quick and tasty recipes.</h2>
-                    </div>
+            <div class="item">
+                <div class="header-text">
+                    <span class="category">Are you cooking? <em>Cook with us.</em></span>
+                    <h2> Hurry! Discover quick and tasty recipes.</h2>
                 </div>
             </div>
+        </div>
+    <div className='login'>
+        <div class="signin"> 
+
+            <div class="content"> 
+                <h2>Register | Login</h2> 
+                    <div class="form"> 
+                        <div class="inputBox"> 
+                            <input type="text" required/> <i>Username</i> 
+                        </div> 
+                        <div class="inputBox"> 
+                            <input type="password" required/> <i>Password</i> 
+                        </div> 
+                {/* <div class="links"> <a href="#">Forgot Password</a> <a href="#">Signup</a> 
+                </div>  */}
+                        <div class="inputBox"> 
+                            <input type="submit" value="Login"/> 
+                        </div> 
+                    </div> 
+                </div> 
+            </div> 
         </div>
     </div>
   )
