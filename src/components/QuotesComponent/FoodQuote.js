ed8fb5ecbd0fabdc67a4fcab01955a7525b96c85
Izet Delibasic <izetdelibasic55@gmail.com>
2023-11-18 18:31:52 +0100

    Creating style for Quotes component

--- src/components/QuotesComponent/FoodQuote.js
+++ src/components/QuotesComponent/FoodQuote.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; 
+import './FoodQuote.css'
 
 const FoodQuote = () => {
     const [quoteData, setQuoteData] = React.useState({
